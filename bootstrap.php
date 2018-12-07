ad2e98b2d17a820db12380b00b7498018fc29fc6
ferhat <ferhat.sobay@yahoo.com.tr>
2018-12-07 14:31:38 +0300

    remove key

--- bootstrap.php
+++ bootstrap.php
@@ -3,7 +3,7 @@
 require 'vendor/autoload.php';
 
 $hosts = [
-    'https://site:49667ddfb3753116d716368190c60350@thorin-us-east-1.searchly.com:443'
+    'https://site:xxxxx@thorin-us-east-1.searchly.com:443'
 ];
 $client = Elasticsearch\ClientBuilder::create()
                     ->setHosts($hosts)
